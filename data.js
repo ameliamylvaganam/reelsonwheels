78dd8b824f47e506eb9a56d9335aef9e6220623f
ameliamylva <41197477+ameliamylvaganam@users.noreply.github.com>
2021-07-01 17:37:41 -0500

    bricks, etc.

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ let data = {
         {
             "title": "the bull painting",
             "date": "30 june 2021",
-            "location": "st. louis, mo: salt n smoke",
+            "location": "st. louis, mo: salt + smoke",
             "text": "today, we went to a rad barbecue joint. st. louis is famous for their barbecue, and our stummies were happy. plus, a little gem fell upon us, graced our eyes and hearts: a painting of a bull, not a few feet from us . . . how sublime! it was colorful and sweet. the bull's face was shy but charming . . . <i>the vision still lingers</i>. anywhomst, we were in a bit of a slump, then the bull and the grass singlehandedly shook us! we recreated the bull's pose. feast your eyes! <br><br><img src = 'library/lizbull.jpg' alt='eliza tilts head to the side and looks directly at camera'> <img src = 'library/ameliabull.jpg' alt='amelia tilts head to the side and looks directly at camera'>"
         },
         {
@@ -12,6 +12,12 @@ let data = {
             "date": "1 july 2021",
             "location": "st. louis, mo: saint louis art museum",
             "text": "we enter a gallery room at the SLAM. 'sketching!' says Eliza. a lady is standing in front of a dark sculpture with a notepad in hand. her pencilstrokes are swift."
+        },
+        {
+            "title": "bricks on bricks on bricks on bricks on bricks on bricks on bricks on bricks on bricks on bric",
+            "date": "1 july 2021",
+            "location": "st. louis, mo: tower grove south",
+            "text": "we walked today more than we have in quite some time, and our path was filled with . . . brick. it surrounded us during our short time here in st. louis! the walk to tea was hot and hard, the walk back a little friendlier. Amelia saw a footprint in the concrete. the man at the mall restuarant gave us two (2) packets of soy sauce, free of charge but certainly not free of kindness."
         },
     ]
 };
